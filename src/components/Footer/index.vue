47afb612e8cb17a03da40dc864002d3e787d741c
zhizhi-ZM <122673157+zhizhi-ZM@users.noreply.github.com>
2023-01-30 13:05:19 +0800

    Update index.vue

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -3,7 +3,7 @@
     <div class="power" v-show="!store.playerState">
       <span
         >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://imsyy.top">無名</a>
+        <a href="https://zhimiao.ml">智喵团队</a>
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
